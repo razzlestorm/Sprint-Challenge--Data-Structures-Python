467e4bc2e0b8e80941d079cc7f7b56dee4451404
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2020-07-31 19:06:06 -0700

    finish ring_buffer with all tests passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.container = []
+        self.overflow = 0
 
     def append(self, item):
-        pass
+        # check if container is at capacity
+        # if not, things can be appended normally
+        if len(self.container) < self.capacity:
+            self.container.append(item)
+        # else, replace the item at the overflow index, and increment overflow
+        elif len(self.container) == self.capacity:
+            self.container[self.overflow] = item
+            if self.overflow == self.capacity-1:
+                self.overflow = 0
+            else:
+                self.overflow += 1
 
     def get(self):
-        pass
+        return self.container
